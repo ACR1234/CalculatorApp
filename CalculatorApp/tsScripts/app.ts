2364ee6fe8fffc5a65e49f6c30cb8abeb331589f
Akhila Chintapally <Akhila Chintapally>
2017-01-11 16:18:37 -0500

    Calculator Application using MVC and Angular 2

---
+++ CalculatorApp/tsScripts/app.ts
@@ -0,0 +1,20 @@
+﻿import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'components/calculator.component.html',
+    styleUrls: ['components/calculator.component.css']
+})
+export class AppComponent {
+    result = 0;
+    no1 = "";
+    no2 = "";
+    title = 'Calculator app - Angular 2';
+    onAddClicked(message: string): void {
+        this.result = parseInt(this.no1) + parseInt(this.no2);
+    }
+    onSubClicked(message: string): void {
+        this.result = parseInt(this.no1) - parseInt(this.no2);;
+    }
+}
